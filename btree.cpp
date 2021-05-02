6a9972bf01a7550ec7975ae0b964306aa5a0d68d
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2021-05-02 01:02:12 -0400

    started insert method

--- btree.cpp
+++ btree.cpp
@@ -1,4 +1,4 @@
-
+#include <algorithm> //used to sort array
 #include "btree.h"
 
 using namespace std;
@@ -55,8 +55,10 @@ int BTree::getHeight()
 	return height;
 }
 
-bool BTree::search (string key)
+bool BTree::search (keyType key)
 {
+	search (key, root, rootAddr);
+	
 	return false;
 }
 
@@ -69,16 +71,21 @@ keyType BTree::retrieve (string key)
 //method to return number of read/writes in part 2
 void BTree::totalio() const
 {
-
+	int totalseeks = read + write;
+	cout << "total seeks: " << totalseeks << endl;
+	cout << "read: " << read << endl;
+	cout << "write: " << write << endl; 
 }
 
 //public countLeaves
 int BTree::countLeaves()
 {
-	return 0;
+	return countLeaves(rootAddr);
 }
 
 //PRIVATE METHODS
+
+//print a preorder traversal of the tree
 void BTree::printTree (int rootAddr)
 {
 	if (rootAddr != -1)
@@ -89,49 +96,100 @@ void BTree::printTree (int rootAddr)
                         printTree(dummy.child[i]);
         }
 }
+
+//print an inorder traversal of the tree
 void BTree::inorder (int rootAddr)
 {
 
 }
-
+//print a postorder traversal of the tree
 void BTree::reverse (int rootAddr)
 {
 
 }
+//find address of the given B-tree node
 int BTree::findAddr (keyType key, BTNode t, int tAddr)
 {
+	
 	return 0;
 }
+//find the address of the parent of the given B-tree node
 int BTree::findpAddr(keyType key, BTNode t, int tAddr)
 {
 	return 0;
 }
+//
 void BTree::insert (keyType key, int recAddr, int oneAddr, int twoAddr)
 {
-
+	BTNode n = getNode(recAddr);
+	//need this to be less than order-1 because initially currSize will be 0,
+	//and after adding a new node, this will be full (at size ORDER-1)
+	if (n.currSize < (ORDER-2))	
+	{
+		//add value to contents array, increment currSize, 
+		//and then sort contents
+		n.contents[n.currSize-1] = key;
+		n.currSize+=1;
+		sort(n.contents, n.contents+n.currSize);
+	}
+	//if not, we will need to split the node and then add the value to the
+	//correct node with the placeNode method	
+	else
+	{
+		splitNode (key,recAddr,oneAddr,twoAddr);
+		placeNode (key,recAddr,oneAddr,twoAddr);
+	}		
 }
+
+//return back the BTNode object given an address
 BTNode BTree::getNode (int recAddr)
 {
 	BTNode n;
+	
+	//get the address of the 
+	treeFile.seekg(recAddr);
+
+	//read in BTNode from the binary file
+	treeFile.read((char *) &n, sizeof(BTNode));
+
 	return n;
 }
+//print out a single node
+//precondition: pass in the address of the record we're observing
+//postcondition: nothing is returned, but the contents of the node are printed
 void BTree::printNode(int recAddr)
 {
-
+	BTNode n;
+	cout << "\t***node of size " << n.currSize << "***" << endl;
+	for (int i = 0; i < n.currSize; i++)
+	{
+		cout << n.contents[i] << endl;	
+	}
 }
+
+//resets the tree/promotes the root after splitting a node
 void BTree::placeNode (keyType k,int recAddr,int oneAddr,int twoAddr)
 {
 
 }
+//method to determine if the current node is a leaf or not
+//precondition: pass in the address of the record
+//postcondition: return a boolean
 bool BTree::isLeaf (int recAddr)
 {
 	BTNode n = getNode(recAddr);
 	return isLeaf(n);
 }
+//method to determine if the current node is a leaf or not
+//precondition: pass in the BTNode of the current record
+//postcondition: return a boolean that tells us if we have a leaf or not
 bool BTree::isLeaf(BTNode t)
 {
 	return t.child[0] == -1 ? true : false;
 }
+//method to count the leaves in a b-tree
+//precondition: pass in the address of the record we're looking
+//postcondition: return an integer with the number of leaves in a b-tree
 int BTree::countLeaves(int recAddr)
 {
 	BTNode node = getNode(recAddr);
@@ -147,17 +205,61 @@ int BTree::countLeaves(int recAddr)
 	}
 	return isLeaf ? 1 : sum;
 }
+
 void BTree::adjRoot (keyType rootElem, int oneAddr, int twoAddr)
 {
 
 }
+
+//method to split a node when we try to add a record to a full node
 void BTree::splitNode (keyType& key,int recAddr,int& oneAddr,int& twoAddr)
 {
 
 }
-bool BTree::search (string key, BTNode t, int tAddr)
+//method to search through the tree when given a transaction file 
+//initially -- pass in the string key (from the transaction file), root, and
+//address of the root 
+//updated -- pass in the album object so that this isn't stuck to only processing
+//Album object
+//postcondition: return a boolean indicating if the value was found or not
+
+bool BTree::search (keyType key, BTNode t, int tAddr)
 {
+	cout << "Searching for " << key << endl;
 	bool found = false;
+	//search through contents to see if content == key; if same, return true.
+	//if key < content, go to the child. 	
+	if (tAddr != -1)
+        {
+		//look through the contents of the node to see if the key can be found
+		for (int i = 0;  i <= t.currSize;  i++)
+                {
+			if (key == t.contents[i])
+			{
+				found = true;
+				break;
+			}
+			else if (key < t.contents[i])
+			{
+				//get the node of the child we are going to explore 
+                		BTNode dummy = getNode(t.child[i]);
+
+				//make a recursive call to search the child node
+				search(key, dummy, t.child[i]);
+			}
+			else if (t.contents[i] < key && (i+1) == t.currSize)
+			{
+				//get the last child that the current node points to
+				//so that we can search it to find our key.
+				BTNode dummy = getNode(t.child[i+1]);
+						
+				//make a recursive call to search the child node
+				search(key, dummy, t.child[i+1]);
+			}
+		}
+        }
+
+	//if leaf and can't find it, return false 
 	return found;
 }
 	
