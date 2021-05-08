e71536df0ef8b579b1c5ca43b5a80faef62e9c93
Emily E. Wilbourn <wilbourn_ee@cobra.cs.mercer.edu>
2021-05-08 10:09:11 -0400

    create BT file

--- createBT.cpp
+++ createBT.cpp
@@ -4,8 +4,13 @@
 //5/8/2021
 #include <iostream>
 #include <fstream>
+#include "album.h" //for album object
+#include "btree.h" //for btree object
+
+
 using namespace std;
 
+BTree createBtree(char* sequ_name, char* btree_name);
 int main(int argc, char *argv[])
 {
 	//create a program that takes the name of the sequential file and the name
@@ -15,8 +20,9 @@ int main(int argc, char *argv[])
 	//sequential file, and name of the B-tree file.
 	if (argc == 3)		
 	{
-		string sequential_name = argv[1];
-		string btree_name = argv[2];	
+		char* sequential_name = argv[1];
+		char* btree_name = argv[2];	
+		BTree btree = createBtree(sequential_name, btree_name);
 	}
 	else
 	{
@@ -24,4 +30,52 @@ int main(int argc, char *argv[])
 		cout << "Enter the name of a sequential file and a B-tree file." << endl;
 	}
 	return 0;
+}
+//driver function for the program
+//precondition: pass in sequential file and btree file names
+//postcondition: btree is createBTree createBtree(char* sequ_name, string btree_name)
+BTree createBtree(char* sequ_name, char* btree_name)
+{
+	fstream input(sequ_name, ios::in);
+	
+	//Album object used to read albums in from our file
+	Album file_album;
+
+	//BTree object to store our BTree
+	BTree b;
+	b.writeHeader(btree_name);
+	int counter = 0;
+	Album first;
+	while(input >> file_album)
+	{
+		cout << "new album: " << file_album << endl;	
+		b.insert(file_album);
+		if(counter == 0)
+		{
+			first = file_album;
+			counter += 1;
+		}
+
+		cout << "Inserted " << file_album << endl;
+		b.printTree();
+		cout << endl << endl;
+	}
+
+	BTNode file_node;
+	fstream binfile(btree_name, ios::in | ios::binary);
+/*
+	//print the contents of binary file
+	cout << "\nPRINTING TREE" << endl;
+	while(binfile.read((char *) &file_node, sizeof(BTNode)))
+	{
+		cout << "Size:" << file_node.currSize << endl;
+		for(int i = 0; i < ORDER-1; i++)
+			cout << file_node.contents[i] << endl;
+	}
+*/
+	cout << "is " << file_album << " in our BTree?" << endl;
+	cout << std::boolalpha << b.search(first) << endl;
+
+	b.printTree();
+	return b;
 }
